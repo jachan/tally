d13da8b45c78fe9f0c996612eb86a9b4d74c90e2
John Chan <johnchan@Administrators-MacBook-Air.local>
2014-05-08 11:40:27 -0500

    Initial Commit

---
+++ Counter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Counter
+//
+//  Created by John Chan on 5/8/14.
+//  Copyright (c) 2014 Chan Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Counter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Counter
+//
+//  Created by John Chan on 5/8/14.
+//  Copyright (c) 2014 Chan Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
